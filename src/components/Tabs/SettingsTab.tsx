c90f2ea04da4cc9be10d28d6bf6cd599b27e817e
Larkinov <buyantolarkinov@gmail.com>
2023-06-21 13:03:52 +0700

    info dialog

--- src/components/Tabs/SettingsTab.tsx
+++ src/components/Tabs/SettingsTab.tsx
@@ -5,10 +5,12 @@ import CountCard from "../Settings/CountCard";
 import ModeGame from "../Settings/ModeGame";
 import Timer from "../Settings/Timer";
 import RandomCard from "../Settings/RandomCard";
+import InfoSettings from "../UI/InfoSettings";
 
 const SettingsTab: React.FC = () => {
   return (
     <Grid container sx={{ overflow: "auto" }} p={1} spacing={2} mt={0}>
+      <InfoSettings/>
       <ModeGame />
       <CountCard />
       <RandomCard/>
---
+++ src/components/UI/InfoSettings.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+
+const InfoSettings = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <div>
+      <Button
+        onClick={handleClickOpen}
+        sx={{ position: "absolute", zIndex: "1", right: "1vw" }}
+      >
+        <InfoOutlinedIcon color="primary" />
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">Информация</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            <b>Режим игры чтение</b> - в этом режиме нет этапа запоминания,
+            поэтому карточки появляются только для чтения. Также здесь
+            отсутствует время для спокойного чтения карточек.
+          </DialogContentText>
+          <DialogContentText id="alert-dialog-description">
+            <b>Режим игры запоминание</b> - после того, как будут прочитаны
+            карточки, начнется этап запоминания - небольшая игра для того чтобы
+            вспомнить порядок слов.
+          </DialogContentText>
+          <DialogContentText id="alert-dialog-description">
+            <b>Количество карточек</b> - можно выбрать весь пакет или какое-то
+            определенно количество
+          </DialogContentText>
+          <DialogContentText id="alert-dialog-description">
+            <b>Случайный порядок</b> - случайный порядок карточек
+          </DialogContentText>
+          <DialogContentText id="alert-dialog-description">
+            <b>Время</b> - играть со временем или без для этапа чтения. После
+            окончания времени начинается этап запоминания
+          </DialogContentText>
+          <DialogActions>
+            <Button onClick={handleClose} autoFocus>
+              Продолжить
+            </Button>
+          </DialogActions>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default InfoSettings;
